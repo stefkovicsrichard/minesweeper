18fb755580e7f9af6398f97c5a1dbc46b41cc676
stefkovicsrichard <stefkovics.richard@ckik.hu>
2025-05-13 20:39:46 +0200

    nem jo

--- script.js
+++ script.js
@@ -1,23 +1,104 @@
 function gen() {
-    const width = parseInt(document.getElementById("width").value);
-    const height = parseInt(document.getElementById("height").value);
-    const div = document.getElementById("doboz");
-    const table = document.createElement("table");
-    table.cWidth = width;
-    table.cHeight = height;
-    table.id = "table"
-    for (let i = 0; i < height; i++) {
-        const tr = document.createElement("tr");
-        for (let j = 0; j < width; j++) {
-            const td = document.createElement("td");
-            td.innerText = i+j;
-            td.isBomb = false;
-            td.id = `${i}_${j}`;
-            tr.appendChild(td);
+    if (document.getElementById("table")) {
+        document.getElementById("table").remove();
+        gen()
+    } else {
+        //#region tablegen
+        const width = parseInt(document.getElementById("width").value);
+        const height = parseInt(document.getElementById("height").value);
+        const chance = parseInt(document.getElementById("chance").value);
+        const div = document.getElementById("doboz");
+        const table = document.createElement("table");
+        table.cWidth = width;
+        table.cHeight = height;
+        table.id = "table"
+        for (let i = 0; i < height; i++) {
+            const tr = document.createElement("tr");
+            for (let j = 0; j < width; j++) {
+                const td = document.createElement("td");
+                td.id = `${i}_${j}`;
+                td.revealed = false;
+                td.number = 0;
+                td.innerText = "##";
+                td.isBomb = Math.floor((Math.random()*100)+1)<=chance;
+                    if (td.isBomb) td.style.backgroundColor = "rgba(255, 0, 0, 0.2)"
+                td.addEventListener("click", () => {clickCheck(td)}, {once: true});
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
         }
-        table.appendChild(tr);
+        div.appendChild(table);
+        //#endregion
+        //remelem a buneim meg megbocsathatoak...
+        //#region numbering
+        for (let i = 0; i < height; i++) {
+            for (let j = 0; j < width; j++) {
+                var cell = document.getElementById(`${i}_${j}`)
+                if (cell.isBomb) cell.number = -1;
+                else if (i == 0 && j == 0) {
+                    if (document.getElementById(`${i}_${j+1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i+1}_${j+1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i+1}_${j}`).isBomb) cell.number++;
+                } else if (i == 0 && j == width-1) {
+                    if (document.getElementById(`${i}_${j-1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i+1}_${j-1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i+1}_${j}`).isBomb) cell.number++;
+                } else if (i == height-1 && j == width-1) {
+                    if (document.getElementById(`${i}_${j-1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i-1}_${j-1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i-1}_${j}`).isBomb) cell.number++;
+                } else if (i == height-1 && j == 0) {
+                    if (document.getElementById(`${i}_${j+1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i-1}_${j+1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i-1}_${j}`).isBomb) cell.number++;
+                } else if (i == 0) {
+                    if (document.getElementById(`${i}_${j-1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i+1}_${j-1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i+1}_${j}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i+1}_${j+1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i}_${j+1}`).isBomb) cell.number++;
+                } else if (j == 0) {
+                    if (document.getElementById(`${i-1}_${j}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i-1}_${j+1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i}_${j+1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i+1}_${j+1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i+1}_${j}`).isBomb) cell.number++;
+                } else if (i == height-1) {
+                    if (document.getElementById(`${i}_${j-1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i-1}_${j-1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i-1}_${j}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i-1}_${j+1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i}_${j+1}`).isBomb) cell.number++;
+                } else if (j == width-1) {
+                    if (document.getElementById(`${i-1}_${j}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i-1}_${j-1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i}_${j-1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i+1}_${j-1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i+1}_${j}`).isBomb) cell.number++;
+                } else {
+                    if (document.getElementById(`${i-1}_${j}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i-1}_${j+1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i}_${j+1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i+1}_${j+1}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i+1}_${j}`).isBomb) cell.number++; 
+                    if (document.getElementById(`${i+1}_${j-1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i}_${j-1}`).isBomb) cell.number++;
+                    if (document.getElementById(`${i-1}_${j-1}`).isBomb) cell.number++;
+                }
+            }
+        }
+        //#endregion
     }
-    div.appendChild(table)
+}
+
+function clickCheck(cell) {
+    if (cell.isBomb) {
+        document.body.style.display = "none";
+        console.log("bomb");
+    } else {
+        cell.revealed = true;
+        console.log("not bomb, num: " + cell.number);
+    }
 }
 
 function isBomb(cell) {
