993dd41e7d875f85341e02bdfee5d768452864ca
stefkovicsrichard <stefkovics.richard@ckik.hu>
2025-05-15 07:54:40 +0200

    idozito nem mukodik + nincs design

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const dirs = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]];
 const mineCounter = document.getElementById("mines");
 var mines = 0;
+var peeking = 0;
+var timer;
+var time;
+var min = 0;
+var sec = 0;
 
 function gen() {
     if (document.getElementById("table")) {
         document.getElementById("table").remove();
         gen()
+        clearInterval(timer);
+        time = "";
     } else {
         //#region tablegen
         const width = parseInt(document.getElementById("width").value);
@@ -32,7 +39,7 @@ function gen() {
                         mines++;
                         mineCounter.innerText = mines;
                     }
-                td.onclick = () => {clickCheck(td)}
+                td.onclick = () => {reveal(td)}
                 td.addEventListener('contextmenu', (event) => {
                     event.preventDefault();
                     flag(td);
@@ -59,7 +66,31 @@ function gen() {
             }
         }
         //#endregion
+        document.getElementById("timer").innerText = "00:00";
+        timer = setInterval(() => {time()}, 1000);
     }
+}
+
+function time() {
+    sec++;
+    if (sec == 60) {
+        sec = 0;
+        min++;
+    }
+    if (min < 10) {
+        if (sec < 10) {
+            timer = `0${min}:0${sec}`
+        } else {
+            timer = `0${min}:${sec}`
+        }
+    } else {
+        if (sec < 10) {
+            timer = `${min}:0${sec}`
+        } else {
+            timer = `${min}:${sec}`
+        }
+    }
+    document.getElementById("timer").innerText = timer;
 }
 
 function isInbounds(i, j, h, w) {
@@ -72,7 +103,7 @@ function peekable(cell) {
         peek(cell);
     }
     cell.onmouseup = () => {
-        unpeek();
+        unpeek(cell);
     };
 }
 
@@ -92,15 +123,21 @@ function flag(cell) {
     }
 }
 
-function clickCheck(cell) {
-    if (!cell.flagged) {
-        cell.onclick = "";
-        if (cell.isMine) {
-            document.body.style.display = "none";
-        } else {
-            reveal(cell);
-        }
-    }
+// function clickCheck(cell) {
+//     if (!cell.flagged) {
+//         cell.onclick = "";
+//         if (cell.isMine) {
+//             document.body.style.display = "none";
+//         } else {
+//             reveal(cell);
+//         }
+//     }
+// }
+
+function lose() {
+    clearInterval(timer);
+    document.getElementById("table").remove();
+    time = "";
 }
 
 function peek(cell) {
@@ -110,32 +147,61 @@ function peek(cell) {
     const cellId = cell.id.split("_");
     const cI = cellId[0]*1;
     const cJ = cellId[1]*1;
-    console.log(cellId, cI, cJ, width, height)
     for (let i = 0; i < dirs.length; i++) {
         if (isInbounds(cI+dirs[i][0], cJ+dirs[i][1], height, width)) {
             var cCell = document.getElementById(`${cI+dirs[i][0]}_${cJ+dirs[i][1]}`);
             if (!cCell.flagged && !cCell.revealed)
-                console.log(dirs[i], cCell);
-                if (!cCell.revealed && !cCell.flagged) cCell.innerText = "_";
+                if (!cCell.revealed && !cCell.flagged) {
+                    cCell.innerText = "_";
+                    peeking++;
+                }
         }
     }
 }
 
-function unpeek() {
-    const table = document.getElementById("table");
-    const height = table.cHeight;
-    const width = table.cWidth;
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            const cCell = document.getElementById(`${i}_${j}`);
-            if (cCell.innerText == "_") {
-                cCell.innerText = "#";
+function unpeek(cell) {
+    // const table = document.getElementById("table");
+    // const height = table.cHeight;
+    // const width = table.cWidth;
+    // for (let i = 0; i < height; i++) {
+    //     for (let j = 0; j < width; j++) {
+    //         const cCell = document.getElementById(`${i}_${j}`);
+    //         if (cCell.innerText == "_") {
+    //             cCell.innerText = "#";
+    //         }
+    //     }
+    // }
+    const cellId = cell.id.split("_");
+    const cI = cellId[0]*1;
+    const cJ = cellId[1]*1;
+    var flagged = 0;
+    for (let i = 0; i < dirs.length; i++) {
+        if (isInbounds(cI+dirs[i][0], cJ+dirs[i][1], height, width)) {
+            var cCell = document.getElementById(`${cI+dirs[i][0]}_${cJ+dirs[i][1]}`);
+            if (cCell.flagged) {
+                flagged++;
             }
         }
     }
+    for (let i = 0; i < dirs.length; i++) {
+        if (isInbounds(cI+dirs[i][0], cJ+dirs[i][1], height, width)){
+            var cCell = document.getElementById(`${cI+dirs[i][0]}_${cJ+dirs[i][1]}`);
+            if (!cCell.flagged && !cCell.revealed) {
+                if (flagged == cell.number) {
+                    reveal(cCell);
+                } else {
+                    cCell.innerText = "#";
+                }
+            }
+        }
+    }
+    peeking = 0;
 }
 
 function reveal(cell) {
+    if (cell.isMine) {
+        lose();
+    }
     if (!cell.revealed) {
         cell.innerText = cell.number;
         cell.onclick = "";
